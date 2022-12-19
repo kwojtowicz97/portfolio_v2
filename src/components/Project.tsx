7a08b9479ececff4065df11ae89f86409cb7f408
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-12-19 22:50:23 +0100

    Added display delay after page loaed

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { TProject } from '../data'
+import React, { useState } from 'react'
+import { TFeature, TProject } from '../data'
+import useInterval from '../hooks/useInterval'
 import ProjectDescription from './ProjectDescription'
 import ProjectsSection from './ProjectsSection'
 
@@ -10,10 +11,25 @@ type TProjectProps = {
 }
 
 const Project = ({ project, initialFeatureIndex, reversed }: TProjectProps) => {
+  const [sectionTranslate, setSectionTranslate] = useState(false)
+  useInterval(() => setSectionTranslate(true), 100)
+  const [currentFeature, setCurrentFeature] = useState(
+    project.features[initialFeatureIndex || 0]
+  )
+
+  const onFeatureClickHandler = (feature: TFeature) => {
+    setCurrentFeature(feature)
+  }
   return (
-    <div className='rounded-3xl bg-gray-200'>
+    <div
+      className={`rounded-3xl bg-gray-200 delay-700 duration-[1500ms] ${
+        sectionTranslate ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
       <ProjectDescription project={project} />
       <ProjectsSection
+        currentFeature={currentFeature}
+        onFeatureClickHandler={onFeatureClickHandler}
         project={project}
         initialFeatureIndex={initialFeatureIndex}
         reversed={reversed}
